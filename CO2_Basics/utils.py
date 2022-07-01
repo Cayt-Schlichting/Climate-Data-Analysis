82ccd34e925a534c1c4e08ebd908b435bd71de6c
Cayt Schlichting <caytschlichting@gmail.com>
2022-07-01 14:13:50 -0700

    save point

--- CO2_Basics/utils.py
+++ CO2_Basics/utils.py
@@ -1,6 +1,9 @@
 #basics
 import numpy as np
 import pandas as pd
+#viz
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 ##### TABLE OF CONTENTS ####
 ## get_co2_data(): acquires and preps data
@@ -31,3 +34,47 @@ def get_co2_data():
     #drop the null months which are on the edges
     dfm = dfm['1964-05':'2022-05']
     return dfm
+
+def mlo_inset_plot(tr_mod,yrdf):
+    '''
+    Plots the monthly and yearly CO2 data, with an inset zoomed in on a two year period.
+    '''
+    fig, ax = plt.subplots(figsize=[12,6])
+    #BIG PLOT
+    ax.plot(tr_mod.index,tr_mod.co2,label='Monthly');
+    ax.plot(yrdf.index,yrdf.co2,label='Yearly');
+    ax.set_title("Observed Mauna Loa $CO_2$ Readings",size=14);
+    ax.set_ylabel("$CO_2$ (ppm)",size=14);
+    ax.autoscale(enable=True, axis='x', tight=True); #makes x axis tight
+    #INSET PLOT
+    dims = [.1, .6, .3,.3] #distance from left axis, distance from y axis, width, height
+    axin = ax.inset_axes(dims) #create new axis
+    axin.plot(tr_mod.co2['1994':'1995'])
+    axin.plot(yrdf.co2['1993':'1995'])
+    axin.set(title="Seasonal Cycle")
+    #inverse colors for inset
+    axin.set(facecolor='white')
+    axin.grid(color='lightgrey')
+    # INSET XLabel Formatting
+    locator = mdates.AutoDateLocator(minticks=3,maxticks=7) #set acceptable tick range
+    formatter = mdates.ConciseDateFormatter(locator) #format with concise dates
+    axin.xaxis.set_major_locator(locator)
+    axin.xaxis.set_major_formatter(formatter)
+    # Draw lines to inset
+    ax.indicate_inset_zoom(axin,edgecolor='black')
+    plt.show()
+    return None
+
+def plot_mlo_seasons(m1_season,m2_season,last_full_year):
+    '''
+    Plots the seasonal variation of CO2 data with the two methods.
+    '''
+    #Show Seasonality
+    plt.figure(figsize=(10,6))
+    plt.bar(range(1,13),m1_season.seas_var, color=np.where(m1_season.seas_var>0, '#0173b2', '#d55e00'),label='Average Variance')
+    plt.plot(range(1,13),m2_season.seasonal[last_full_year],label='Statsmodel Decomposition',marker='x',color='k')
+    plt.legend()
+    plt.xticks(range(1,13),labels=m1_season.mn);
+    plt.ylabel("CO$_2$ Variance (ppm)");
+    plt.title("Average Monthly CO$_2$ Variance",size=14);
+    return None
