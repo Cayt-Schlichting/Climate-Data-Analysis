b445e3b421a169eb5e189bb72de9668926062c51
Cayt Schlichting <caytschlichting@gmail.com>
2022-07-04 22:21:54 -0700

    additional plotting functions added

--- CO2_Basics/utils.py
+++ CO2_Basics/utils.py
@@ -62,6 +62,38 @@ def mlo_inset_plot(tr_mod,yrdf):
     axin.xaxis.set_major_formatter(formatter)
     # Draw lines to inset
     ax.indicate_inset_zoom(axin,edgecolor='black')
+    plt.legend()
+    plt.show()
+    return None
+
+def mlo_adj_inset_plot(tr_mod,yrdf):
+    '''
+    Plots the monthly and sesasonally adjusted CO2 data, with an inset zoomed in on a two year period.
+    '''
+    #Plot observed and seasonally adjusted
+    fig, ax = plt.subplots(figsize=[12,6])
+    #BIG PLOT
+    ax.plot(tr_mod.co2,label='Observed')
+    ax.plot(tr_mod.co2_adj,label='Seasonally Adjusted')
+    ax.set_ylabel("CO$_2$ (ppm)")
+    ax.set_title("Seasonally Adjusted CO$_2$ Readings",size=14);
+    ax.autoscale(enable=True, axis='x', tight=True)
+    #INSET PLOT
+    axin = ax.inset_axes([.1, .6, .3,.3]) 
+    axin.plot(tr_mod.co2['1994':'1995'],label='Observed')
+    axin.plot(tr_mod.co2_adj['1994':'1995'],label='Seasonally Adjusted')
+    axin.set(title="Seasonal Cycle")
+    #inverse colors
+    axin.set(facecolor='white')
+    axin.grid(color='lightgrey')
+    # INSET XLabel Formatting
+    locator = mdates.AutoDateLocator(minticks=3,maxticks=7) #set acceptable tick range
+    formatter = mdates.ConciseDateFormatter(locator) #format with concise dates
+    axin.xaxis.set_major_locator(locator)
+    axin.xaxis.set_major_formatter(formatter)
+    # Draw lines to inset
+    ax.indicate_inset_zoom(axin,edgecolor='black')
+    plt.legend(loc=4)
     plt.show()
     return None
 
@@ -78,3 +110,38 @@ def plot_mlo_seasons(m1_season,m2_season,last_full_year):
     plt.ylabel("CO$_2$ Variance (ppm)");
     plt.title("Seasonal Trends in CO$_2$ Observations",size=14);
     return None
+
+def plot_models(te_pred):
+    '''
+    Plots the 3 models created against the observed values
+    '''
+    #Do some plotting
+    plt.figure(figsize=(12,5))
+    # plt.plot(tr_mod_eom.co2,label='Train')
+    plt.plot(te_pred.co2,label='Observed Values')
+    plt.plot(te_pred.h_linear_w_seas, label='Holt - Linear')
+    plt.plot(te_pred.h_exp_w_seas, label='Holt - Exponential')
+    plt.plot(te_pred.hw_ExpSmooth, label='Holt-Winters Exponential Smoothing')
+    plt.title("Predicted Mauna Loa CO$_2$ Readings", size=14)
+    plt.ylabel("CO$_2$ (ppm)")
+    plt.legend()
+    plt.show()
+    return None
+    
+def get_custom_error(te_pred,predictions):
+    '''
+    Given the test predictions from a time series model, and forecasted predictions, this extrapolates an error band 
+    onto the forecasted data based off of the error found in the test dataset.
+    '''
+    #Calculate error at given point
+    te_pred['err'] = round((te_pred.co2 - te_pred['h_exp_w_seas']),2)
+    #Percent error at given point
+    te_pred['perc_err'] = round((te_pred.err / te_pred.co2),4)
+    #create trendline
+    m, b = np.polyfit(range(0,te_pred.shape[0]),te_pred.perc_err,1)
+    #create substitute x values for prediction dataframe
+    x = range(1,predictions.shape[0]+1)
+    #Generate extrapolated error predictions
+    #M*x = percent error at given point.  *Pred gets the expected error in ppm
+    error = m*x*predictions.pred_w_seas
+    return error
